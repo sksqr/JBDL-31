aaa28ff278f4ca8ddee47a6802b5804e46ea7f1c
sksqr <shashikant3108@gmail.com>
2022-06-12 21:47:23 +0530

    L17 code: Oauth2 with GitHub, Kafka Intro

---
+++ L17_Oauth2App/src/main/java/com/example/Config.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class Config extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .logout()
+                .logoutSuccessUrl("/").permitAll()
+                .and()
+                .oauth2Login();
+    }
+}
---
+++ L17_Oauth2App/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,14 @@
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+}
